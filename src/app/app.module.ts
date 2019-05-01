385a64e33b414c05b7a2cb2239bffe7cba63a66a
jordanavi <jordanavilela01@gmail.com>
2019-05-01 16:42:41 -0300

    Criando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AreaConhecimentoComponent } from './area-conhecimento/area-conhecimento.component';
+import { ConteudoComponent } from './conteudo/conteudo.component';
+import { DisciplinaComponent } from './disciplina/disciplina.component';
+import { ProfessorComponent } from './professor/professor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AreaConhecimentoComponent,
+    ConteudoComponent,
+    DisciplinaComponent,
+    ProfessorComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/area-conhecimento/area-conhecimento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-area-conhecimento',
+  templateUrl: './area-conhecimento.component.html',
+  styleUrls: ['./area-conhecimento.component.css']
+})
+export class AreaConhecimentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
