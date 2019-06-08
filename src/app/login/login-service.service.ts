cdce9f81e0c4953ce49355a1a8157f1795026d47
jordanavi <jordanavilela01@gmail.com>
2019-06-08 10:12:40 -0300

    Conteudo da classe servico

--- src/app/login/login-service.service.ts
+++ src/app/login/login-service.service.ts
@@ -1,9 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from './user';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
+
 export class LoginServiceService {
 
-  constructor() { }
+  public showNavbarEmmitter: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  private authenticated = false;
+
+  constructor(private router: Router) { }
+
+  signIn(user: User){
+    if((user.email === 'user@email.com' || user.email === 'usuario@email.com') && user.password === '123456'){
+      this.authenticated = true;
+      this.showNavBar(true);
+      this.router.navigate(['/']);
+    }
+    else{
+      this.authenticated = false;
+    }
+  }
+
+  logout(){
+    this.authenticated = false;
+    this.showNavBar(false);
+    this.router.navigate(['/signin']);
+  }
+
+  isAuthenticated(){
+    return this.authenticated;
+  }
+
+  private showNavBar(ifShow: boolean){
+    this.showNavbarEmmitter.emit(ifShow);
+  }
 }
--- src/app/login/user.ts
+++ src/app/login/user.ts
@@ -1,2 +1,5 @@
 export interface User {
+
+    email: string;
+    password: string;
 }
