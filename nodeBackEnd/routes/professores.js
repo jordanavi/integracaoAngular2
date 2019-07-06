f8d14ca5dadd0d21b5ffbfd8a2285e9df912ce54
jordanavi <jordanavilela01@gmail.com>
2019-07-06 18:51:41 -0300

    Deletar o recurso e testando com o Postman

--- nodeBackEnd/routes/professores.js
+++ nodeBackEnd/routes/professores.js
@@ -37,4 +37,15 @@ router.get('/search/params', (req, res) => {
         .catch(err => this.console.log(err));
 })
 
+router.delete("/:id", (req, res) =>{
+    Professor.destroy({
+        where: {
+            codigo: req.params.id
+        }
+    })
+    .then(result => res.sendStatus(204))
+    .catch(error => {
+        res.status(412).json({msg: error.message});
+        });
+})
 module.exports = router;
