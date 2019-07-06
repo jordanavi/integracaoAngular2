b3cbec397b3cb854623a1702476eaa7eff3998cb
jordanavi <jordanavilela01@gmail.com>
2019-07-06 15:52:52 -0300

    instalando as dependencias para o nodejs - pagina 14

---
+++ nodeBackEnd/app.js
@@ -0,0 +1,9 @@
+const express = required('express');
+
+const PORT = 3000;
+
+const app = express();
+
+app.get("/", (resp,res) => res.json({status: "Nodejs backend"}));
+
+app.listen(PORT,()=>console.log("Escutando na porta "+PORT));
