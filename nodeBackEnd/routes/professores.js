6c70c0e14cfe858805a55c4ff0ba9933fe622002
jordanavi <jordanavilela01@gmail.com>
2019-07-06 18:31:46 -0300

    Criando consulta pelo pelo codigo - pagina 34

--- nodeBackEnd/routes/professores.js
+++ nodeBackEnd/routes/professores.js
@@ -9,4 +9,19 @@ router.get("/", (req,res)=>
             res.status(412).json({msg: error.message});
 }));
 
+router.get("/:id", (req,res)=>{
+    Professor.findOne({
+        where: {
+            codigo: req.params.id,
+        }
+    }).then(result => {
+        if(result){
+            res.json(result);
+        } else {
+            res.sendStatus(404);
+        }
+        }).catch(error => {
+            res.status(412).json({msg: error.message});
+});
+})
 module.exports = router;
