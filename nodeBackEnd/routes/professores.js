a6a7aca2c0b4657860a7bdbce1495511e1e8d768
jordanavi <jordanavilela01@gmail.com>
2019-07-06 18:21:34 -0300

    Alterando clientes.js de routes para chamar o banco - pag 32

--- nodeBackEnd/routes/professores.js
+++ nodeBackEnd/routes/professores.js
@@ -1,13 +1,12 @@
 const express = require("express");
+const Professor = require('../models/professores');
 const router = express.Router();
 
-router.get("/", (req,res)=>{
-    res.json(
-        [
-            {'codigo':1,'nome':'jose'},
-            {'codigo':2,'nome':'maria'}
-        ]
-    )
-});
+router.get("/", (req,res)=>
+    Professor.findAll()
+        .then(result => res.json(result))
+        .catch(error =>{
+            res.status(412).json({msg: error.message});
+}));
 
 module.exports = router;
