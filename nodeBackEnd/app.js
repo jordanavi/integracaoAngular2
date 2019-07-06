f87995877c1903480241e0e1e9fae9deba2425f3
jordanavi <jordanavilela01@gmail.com>
2019-07-06 18:06:53 -0300

    Verificacao do banco de dados relacional na camada model

--- nodeBackEnd/app.js
+++ nodeBackEnd/app.js
@@ -3,6 +3,11 @@ const express = require('express');
 const PORT = 3000;
 
 const app = express();
+const db = require('./config/database');
+
+db.authenticate()
+    .then(()=>console.log('Database connected...'))
+    .catch(err =>console.log('Error: '+err))
 
 app.set("json spaces", 4);
 
@@ -11,6 +16,7 @@ const professores = require('./routes/professores');
 
 app.use('/',index);
 app.use('/professores',professores);
+
 
 
 // app.get("/", (resp,res) => res.json({status: "Nodejs backend"}));
