98929586d30c4383e0080d9092eeced9edf3cf7a
jordanavi <jordanavilela01@gmail.com>
2019-07-06 16:23:59 -0300

    Adicionando um recurso estatico no professor - pagina 18

--- nodeBackEnd/app.js
+++ nodeBackEnd/app.js
@@ -6,4 +6,13 @@ const app = express();
 
 app.get("/", (resp,res) => res.json({status: "Nodejs backend"}));
 
+app.get("/professores", (req,res) =>{
+    res.json(
+        [
+            {'codigo':1,'nome':'joao'},
+            {'codigo':2,'nome':'maria'}
+        ]
+    )
+});
+
 app.listen(PORT,()=>console.log("Escutando na porta "+PORT));
